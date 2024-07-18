22a3921a7256719b4075b3cca7bf6f534bfbe6d3
akashdey23 <akashdey9128@gmail.com>
2024-07-19 00:53:00 +0530

    product page on each click of product

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './Item.css';
+import { Link } from "react-router-dom";
 
 const Item = ({id,name,image,new_price,old_price}) => {
   return (
     <div className="item" id={id}>
-      <img src={image} alt={name} />
+      <Link to={`/product/${id}`}><img src={image} alt={name} /></Link>
       <p>{name}</p>
       <div className="item-prices">
         <div className="item-price-new">${new_price}</div>
